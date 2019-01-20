370eb624bffb9d5f73461e304f6116c52499bfa0
Noah <noahleviwilder@gmail.com>
2019-01-20 13:19:15 -0500

    Initial Commit

---
+++ DrawingLines/DrawingLines.h
@@ -0,0 +1,19 @@
+//
+//  DrawingLines.h
+//  DrawingLines
+//
+//  Created by Noah Wilder on 2019-01-20.
+//  Copyright © 2019 Noah Wilder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DrawingLines.
+FOUNDATION_EXPORT double DrawingLinesVersionNumber;
+
+//! Project version string for DrawingLines.
+FOUNDATION_EXPORT const unsigned char DrawingLinesVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DrawingLines/PublicHeader.h>
+
+
